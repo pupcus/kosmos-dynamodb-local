32191d4beae70890e367b64baa27947b13e19281
Michael Pendergrass <mdpendergrass@gmail.com>
2017-10-31 16:49:58 -0400

    Version 0.0.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kosmos/kosmos-dynamodb-local "0.0.3"
+(defproject kosmos/kosmos-dynamodb-local "0.0.4-SNAPSHOT"
 
   :description "kosmos local dynamodb database component (for testing)"
 
