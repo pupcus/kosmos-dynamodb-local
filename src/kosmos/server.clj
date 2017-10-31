eb849df4da82dcc840ed667a86af7a3879b5fa70
Michael Pendergrass <mdpendergrass@gmail.com>
2017-10-31 16:49:13 -0400

    remove unnecessary println statements

--- src/kosmos/server.clj
+++ src/kosmos/server.clj
@@ -3,16 +3,12 @@
             [com.stuartsierra.component :as component]
             [kosmos.server.util :as util]))
 
-
-
 (defrecord LocalDynamoDbServerComponent [cors port in-memory? shared-db? db-path optimize-db-before-startup? delay-trainsient-statuses?]
   component/Lifecycle
   (start [{:keys [in-memory? db-path] :as component}]
     (log/info "starting local amazon dynamodb server instance for testing ...")
-    (println "COMPONENT="component)
     (assert (not (and in-memory? db-path)) "cannot specify in memory database AND a database directory at the same time")
     (let [command-line-args (util/build-command-line-args component)
-          _ (println command-line-args)
           native-directory (util/setup-native-libraries)]
       (when db-path (util/ensure-directory-exists db-path))
       (let [server (com.amazonaws.services.dynamodbv2.local.main.ServerRunner/createServerFromCommandLineArgs
